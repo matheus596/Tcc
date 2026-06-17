57b9a6d171113412a2e73191b2884831a1e6c334
matheus596 <124275875+matheus596@users.noreply.github.com>
2026-06-17 13:40:30 -0300

    Remove database configuration from config.php
    Removed database configuration constants from config.php.

--- config.php
+++ config.php
@@ -1,6 +1,1 @@
-<?php
-define('DB_HOST', 'sql107.infinityfree.com');    // MYSQL HOSTNAME
-define('DB_USER', 'if0_41305518');              // MYSQL USERNAME
-define('DB_PASS', 'Matheusoficial');            // MYSQL PASSWORD (clica no olho 👁️ pra ver)
-define('DB_NAME', 'if0_41305518_usuarios');     // MYSQL DATABASE NAME
-?>
+
